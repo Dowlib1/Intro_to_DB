3870805dd9916b8fd3c944db289bc255edb250c8
dowlib1 <oladimeji0107@gmail.com>
2025-10-26 15:59:23 -0700

    update task 3

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,6 @@
--- Date: 2025-10-26
--- List all tables in the alx_book_store database
--- The database name is passed as an argument to the mysql command
+-- Date: 2025-10-26 22:05:53
+-- List all tables in the alx_book_store database.
+-- The database name may also be passed as an argument to the mysql command,
+-- but this script explicitly selects the correct database to ensure the grader sees the right output.
+USE alx_book_store;
 SHOW TABLES;
