44da1ae21d884f19ce0ca337f1df50f69bad5c91
dowlib1 <oladimeji0107@gmail.com>
2025-10-26 15:00:12 -0700

    updating mysqlserve

--- task_4.sql
+++ task_4.sql
@@ -1,18 +1,17 @@
--- Full description of the 'books' table.
--- The database name will be passed as an argument of the mysql command.
--- This script retrieves column metadata from INFORMATION_SCHEMA to show a complete table description.
-
+-- Date: 2025-10-26 14:56:52 -0700
+-- Full description of the 'books' table using INFORMATION_SCHEMA.
+-- The database name will be passed as an argument to the mysql command.
 USE alx_book_store;
 
 SELECT
-  COLUMN_NAME AS 'Field',
-  COLUMN_TYPE AS 'Type',
-  IS_NULLABLE AS 'Null',
-  COLUMN_KEY AS 'Key',
-  COLUMN_DEFAULT AS 'Default',
-  EXTRA AS 'Extra',
-  COLUMN_COMMENT AS 'Comment'
+  COLUMN_NAME AS Field,
+  COLUMN_TYPE AS Type,
+  IS_NULLABLE AS `Null`,
+  COLUMN_KEY AS `Key`,
+  COLUMN_DEFAULT AS `Default`,
+  EXTRA AS Extra,
+  COLUMN_COMMENT AS Comment
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = DATABASE()
   AND TABLE_NAME = 'books'
-ORDER BY ORDINAL_POSITION;;
+ORDER BY ORDINAL_POSITION;
